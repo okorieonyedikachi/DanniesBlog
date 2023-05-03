c87866be40554e5de350000cb56b5f87a798ab3a
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-05-03 12:43:39 +0100

    Installed react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Addpost from './pages/Addpost.jsx';
+import Posts from './pages/Posts.jsx';
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Addpost />
+  },
+  {
+    path:"/posts",
+    element: <Posts />
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider
+      router={router}
+    />
   </React.StrictMode>,
 )
