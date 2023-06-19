86fa4d668efa232c0b265bde0c4d3ca90c52cf75
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-06-19 16:59:23 +0100

    redirect to signup newletter

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const https = require("https")
 const PORT = 3000;
 
 const app = express();
-// app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static('public'))
 
-app.get( "/", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/homepage.html")
 })
+
+app.get("/signUp", (req, res) => {
+    res.sendFile(__dirname + "/public/signup.html")
+})
+
+app.post ("/signUp", (req, res) => {
+    res.redirect("/signUp") 
+})
+
 
 app.listen(PORT,() => {"Server is running on port 3000"})
