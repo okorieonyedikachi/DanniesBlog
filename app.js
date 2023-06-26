817568d546128c81876eeac4c7f36c92d48cab12
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-06-26 11:09:02 +0100

    icons import

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const express = require("express");
 const https = require("https")
 const PORT = 3000;
 
+
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore} = require('firebase-admin/firestore');
+
+const serviceAccount = require("./dannies-newsletter-firebase-adminsdk-2bfzr-3dc1eca0b8.json");
+
+
+// Initialization of Cloud firestore(firebase)
+initializeApp({
+    credential:cert(serviceAccount)
+  });
+  
+
+const db = getFirestore();
+const usersCollection = db.collection('users')
+const newUserRef = usersCollection.doc();
+
+
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -18,11 +36,16 @@ app.get("/signUp", (req, res) => {
 
 app.post ("/signUp", (req, res) => {
     res.redirect("/signUp") 
+    
     const firstName = req.body.fName
     const lastName = req.body.lName
     const email = req.body.email
 
-    console.log(firstName, lastName, email)
+    newUserRef.set({
+    FirstName: firstName,
+    LastName: lastName,
+    Email: email
+    });
     
 })
 
