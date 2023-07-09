5313232b2a64bbcfa81e35de5bd49a8571046459
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-07-09 19:45:34 +0100

    Editor Page : Firebase initialization and accessing firestore

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 
-const { initializeApp} = require('firebase-admin/app');
-const { getDatabase} = require('firebase/database');
-
+const { initializeApp } = require("firebase-admin/app");
+const { getDatabase } = require("firebase/database");
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,18 +16,63 @@ const firebaseConfig = {
   messagingSenderId: "189740482664",
   appId: "1:189740482664:web:3cfcd7c4113646bd753bdd",
 
-  databaseURL: "https://dannies-newsletter-default-rtdb.firebaseio.com/"
-
+  // databaseURL: "https://dannies-newsletter-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const database = getDatabase(app);
+const db = firebase.firestore();
 
 const titleField = document.querySelector(".title");
 const articleField = document.querySelector(".article");
-const publishBtn = document.querySelector('.publish-btn');
+// const publishBtn = document.querySelector(".publish-btn");
 
 
-console.log('dog')
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+document.getElementById('publish-btn').addEventListener("click", () => {
+  if (articleField.value.length && titleField.value.length) {
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    let blogTitle = titleField.value.split(" ").join("-");
+    let blogId = "";
+    for (let i = 0; i < 4; i++) {
+      id += letters[Math.floor(Math.random() * letters.length)];
+    }
+    // setting up docName for the firestore
+
+    let docName = `${blogTitle} - ${blogId}`;
+    let date = new Date();
+
+    //Accessing firestore
+    db.collection("Blogs")
+      .doc("docName")
+      .set({
+        title: titleField.value,
+        article: articleField.value,
+        bannerImage: bannerPath,
+        publishedAt: `${date.getDate()} ${
+          months[date.getMonth()]
+        } ${date.getFullYear()}`,
+      })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
