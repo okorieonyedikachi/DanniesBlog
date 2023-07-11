79c0d5a5b0b2800c4664fd5f09d1a8c8e818f18b
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-07-11 14:41:28 +0100

    FIrebase configuration

---
+++ public/js/editor.mjs
@@ -0,0 +1,6 @@
+import app from "./firebase.js"
+
+const titleField = document.querySelector(".title");
+const articleField = document.querySelector(".article");
+const publishBtn = document.querySelector(".publish-btn");
+
---
+++ public/js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZhz4m8nw-35c0YkCiGoKdRJo14TA3Tzk",
+  authDomain: "dannies-newsletter.firebaseapp.com",
+  databaseURL: "https://dannies-newsletter-default-rtdb.firebaseio.com",
+  projectId: "dannies-newsletter",
+  storageBucket: "dannies-newsletter.appspot.com",
+  messagingSenderId: "189740482664",
+  appId: "1:189740482664:web:3cfcd7c4113646bd753bdd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app
