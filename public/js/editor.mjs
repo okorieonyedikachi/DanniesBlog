35082828d49e8d810d355880b64e7e0bee3752bd
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-07-12 22:02:09 +0100

    deleted file

--- public/js/editor.mjs
+++
@@ -1,6 +0,0 @@
-import app from "./firebase.js"
-
-const titleField = document.querySelector(".title");
-const articleField = document.querySelector(".article");
-const publishBtn = document.querySelector(".publish-btn");
-
