b2919c9f6ac1641f49892fd7dfe11d1048e988cc
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-07-12 22:03:13 +0100

    Tried firebase config on client-side .. it didn't work

---
+++ public/js/editor.js
@@ -0,0 +1,28 @@
+
+
+
+// const titleField = document.querySelector(".title");
+// const articleField = document.querySelector(".article");
+// const publishBtn = document.querySelector(".publish-btn");
+
+// let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+// publishBtn.addEventListener("click", async () => {
+   
+//   await setDoc(doc(db, "Blog", " "), {
+//     title: titleField.value,
+//     article: articleField.value,
+//   });
+      
+//   then(() => {
+//     console.log("Document successfully written!");
+//   })
+//   .catch((error) => {
+//   console.error("Error writing document: ", error);
+//   });
+
+  
+    
+
+  
+// })
